2c649c72aa9ffdb871a685865709fc133587c63b
agualbbus <agualbbus@yahoo.com.ar>
2015-06-01 14:30:05 -0300

    github pages

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var gutil = require('gutil');
+
+
+//coffee
+gulp.task('coffee', function() {
+  gulp.src('./src/coffee/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./src/js'))
+});
+
+
+//SASS
+gulp.task('sass', function () {
+  gulp.src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/*.scss', ['sass']);
+});
+
+
+
+
+
+
+//SERVER
+gulp.task('server', function() {
+  gulp.src('/.')
+    .pipe(server({
+      livereload: true,
+      directoryListing: true,
+      open: true,
+      port: 3030,
+      defaultFile: 'index.html'
+    }));
+});
+
+
+//default
+gulp.task('default',['sass','coffee']);
