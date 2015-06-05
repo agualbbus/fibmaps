3dc6d7fcf7f7c488f9e8b0e2b3c2ac97287103d2
agualbbus <agualbbus@yahoo.com.ar>
2015-06-05 10:45:20 -0300

    init interact

--- src/js/main.js
+++ src/js/main.js
@@ -61,15 +61,57 @@ $(function(){
 
     $sizeFib.val( $fibCont.width() );
 
-    $fibCont.resizable({
-        handles: 'n, e, s, w',
-        aspectRatio: true,
-        resize: function(event, ui){
-            $sizeFib.foundation('slider', 'set_value', $fibCont.width() * 100 / ($(window).width()*1.25)  );
-        }
-    })
-    .draggable();
+//    $fibCont.resizable({
+//        handles: 'n, e, s, w',
+//        aspectRatio: true,
+//        resize: function(event, ui){
+//            $sizeFib.foundation('slider', 'set_value', $fibCont.width() * 100 / ($(window).width()*1.25)  );
+//        }
+//    })
+//    .draggable();
+// target elements with the "draggable" class
+    interact('#fib-cont')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
 
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+        onend: function (event) {
+          var textEl = event.target.querySelector('p');
+
+          textEl && (textEl.textContent =
+            'moved a distance of '
+            + (Math.sqrt(event.dx * event.dx +
+                         event.dy * event.dy)|0) + 'px');
+        }
+      });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing demo
+  window.dragMoveListener = dragMoveListener;
     var rot = {
         z: 0,
         y: 0,
