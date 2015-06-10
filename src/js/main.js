19c16a8168dc15d1cc9cd45a5adacd7f16c99ebf
agualbbus <agualbbus@yahoo.com.ar>
2015-06-10 15:53:00 -0300

    an aproach to gmaps pixels to bounds

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,22 @@
 //maps :AIzaSyBVbAsOk4aQc12Yll-7KazLzLnY1GkMRZY
 
 if (typeof google !== "undefined"){
-
+    var overlay;
+    var fibDiv = document.getElementById('translate-container');
     var cataratas =new google.maps.LatLng(-25.665913, -54.448626);
 
-    function MapsInitialize() {
-        var mapOptions = {
+    var mapDiv = document.getElementById('map-canvas');
+    var mapOptions = {
           center: { lat: -34.397, lng: 150.644},
           zoom: 13,
           panControl: true,
           rotateControl: true
-        };
-        var mapDiv = document.getElementById('map-canvas')
-        var map = new google.maps.Map(mapDiv, mapOptions);
+    };
+    var map = new google.maps.Map(mapDiv, mapOptions);
+
+
+    function MapsInitialize() {
+
 
         // Try W3C Geolocation (Preferred)
         if(navigator.geolocation) {
@@ -42,19 +46,19 @@ if (typeof google !== "undefined"){
         }
 
         //overlay
-        var fibDiv = document.getElementById('translate-container');
+
         var swBound = new google.maps.LatLng(-22.92804166565176, -43.23171615600586);
         var neBound = new google.maps.LatLng(-22.88503184835787, -43.15824508666992);
         var bounds = new google.maps.LatLngBounds(swBound, neBound);
 
-        overlay = new fibOverlay(bounds, fibDiv, map);
+        //overlay = new fibOverlay(bounds, fibDiv, map);
         //click event ltnr
         google.maps.event.addListener(map, 'click', function(e){
             console.log(e.latLng);
         });
 
+        mapDiv.appendChild(fibDiv);
     }
-
 
     google.maps.event.addDomListener(window, 'load', MapsInitialize);
 }
@@ -66,204 +70,217 @@ if (typeof google !== "undefined"){
 
 $(function(){
     $(document).foundation();
-//    var $fibCont = $('#fib-cont'),
-//        $fibSvg = $('#fib-svg'),
-//        $fibPath = $('#fib-cont path, #fib-cont use'),
-//        $flipV = $('#flipV'),
-//        $flipH = $('#flipH'),
-//        $rotate = $('#rotate'),
-//        $hideFib = $('#hideFib'),
-//        $sizeFib = $('#sizeFib'),
-//        $transCont = $('#translate-container');
-//
-//
-//
-////    $fibCont.resizable({
-////        handles: 'n, e, s, w',
-////        aspectRatio: true,
-////        resize: function(event, ui){
-////            $sizeFib.foundation('slider', 'set_value', $fibCont.width() * 100 / ($(window).width()*1.25)  );
-////        }
-////    })
-////    .draggable();
-//// target elements with the "draggable" class
-//
-//
-//    interact('#translate-container')
-//      .draggable({
-//        // enable inertial throwing
-//        //inertia: true,
-//        // keep the element within the area of it's parent
-////        restrict: {
-////          restriction: "parent",
-////          endOnly: true,
-////          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-////        },
-//
-//        // call this function on every dragmove event
-//        onmove: function(event){
-//            var target = event.target,
-//                // keep the dragged position in the data-x/data-y attributes
-//                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-//                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-//            //apply transform
-//            transform.trans = {
-//                x: x,
-//                y: y
-//            }
-//
-//            transformIt();
-//
-//            // update the posiion attributes
-//            target.setAttribute('data-x', x);
-//            target.setAttribute('data-y', y);
-//
-//        },
-//        // call this function on every dragend event
-//        onend: function (event) {
-//
-//          var textEl = event.target.querySelector('p');
-//
-//          textEl && (textEl.textContent =
-//            'moved a distance of '
-//            + (Math.sqrt(event.dx * event.dx +
-//                         event.dy * event.dy)|0) + 'px');
-//        }
-//      });
-//
-//
-//
-//
-//
-//
-//    //use this obj to store transforms values
-//    var transform = {
-//        rot: { z: 0, y:0, x:0},
-//        trans: {x:0, y:0}
-//    };
-//
-//
-//
-//    //CLICKS AND CHANGES
-//
-//    $flipV.on('click', function(e){
-//        transform.rot.x = ( transform.rot.x==0 ? 180 : 0 );
-//        transformIt();
-//    });
-//
-//    $flipH.on('click', function(e){
-//        transform.rot.y = ( transform.rot.y==0 ? 180 : 0 );
-//        transformIt();
-//    });
-//
-//    $hideFib.on('click', function(e){
-//        $fibCont.toggleClass('disable');
-//        $(this).html( ( $(this).html()=="Hide" ? "Show" : "Hide"  ) );
-//    });
-//
-//
-//    $rotate.find('.inc').on('click',function(){
-//        var val = parseFloat($rotate.attr('data-slider')) + .5;
-//        $rotate.foundation('slider', 'set_value', val );
-//    });
-//
-//    $rotate.find('.dec').on('click',function(){
-//        var val = parseFloat($rotate.attr('data-slider')) - .5;
-//        $rotate.foundation('slider', 'set_value', val );
-//    });
-//
-//
-//    $rotate.on('change.fndtn.slider', function(e){
-//        var deg = parseFloat( $(this).attr('data-slider') );
-//            //deg = per * 360 / 100 - 180;
-//        transform.rot.z = deg;
-//        transformIt();
-//    });
-//
-//
-//
-//    $sizeFib.find('.inc').on('click',function(){
-//        var val = parseFloat($sizeFib.attr('data-slider')) + 0.2;
-//        $sizeFib.foundation('slider', 'set_value', val );
-//    });
-//
-//    $sizeFib.find('.dec').on('click',function(){
-//        var val = parseFloat($sizeFib.attr('data-slider')) - 0.2;
-//        $sizeFib.foundation('slider', 'set_value', val );
-//    });
-//
-//    $sizeFib.on('change.fndtn.slider', function(e){
-//        var per = parseFloat( $(this).attr('data-slider') ),
-//            size = per * ($(window).width() * 1.25) / 100;
-//        resizeFib(size);
-//    });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    function resizeFib(val){
-//        $fibCont.width(val);
-//        $transCont.css({
-//            'width': $fibCont.width(),
-//            'height': $fibCont.height(),
-//        } );
-//
-//    }
-//
-//    function tOrigin(){
-//      var x,y,
-//          l='left',
-//          r='right',
-//          t='top',
-//          b='bottom';
-//      if(transform.rot.x == 0 && transform.rot.y == 0){
-//        x = l;
-//        y = b;
-//      }
-//      else if(transform.rot.x == 180 && transform.rot.y == 180){
-//        x = r;
-//        y = t;
-//      }
-//      else if(transform.rot.x == 180 && transform.rot.y == 0){
-//        x = l;
-//        y = t;
-//      }
-//      else if(transform.rot.x == 0 && transform.rot.y ==180 ){
-//        x = r;
-//        y = b;
-//      }
-//
-//      return {
-//        x: x,
-//        y: y
-//      }
-//    }
-//
-//
-//    function transformIt(){
-//
-//        var origin = tOrigin();
-//
-//        $fibSvg.css('transform', 'rotateX(' +transform.rot.x+ 'deg) rotateY(' +transform.rot.y+ 'deg)');
-//
-//        $fibCont.css({
-//          'transform': 'rotateZ(' +transform.rot.z+ 'deg)',// translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
-//          'transform-origin': tOrigin().x+' '+tOrigin().y
-//        });
-//
-//        $transCont.css({
-//            'transform': 'translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
-//            'width': $fibCont.width(),
-//            'height': $fibCont.height(),
-//        } );
-//
-//    }
+    var $fibCont = $('#fib-cont'),
+        $fibSvg = $('#fib-svg'),
+        $fibPath = $('#fib-cont path, #fib-cont use'),
+        $flipV = $('#flipV'),
+        $flipH = $('#flipH'),
+        $rotate = $('#rotate'),
+        $hideFib = $('#hideFib'),
+        $sizeFib = $('#sizeFib'),
+        $transCont = $('#translate-container'),
+        $lock = $('#lock');
+
+
+
+
+
+    interact('#translate-container')
+      .draggable({
+
+        // call this function on every dragmove event
+        onmove: function(event){
+            var target = event.target,
+                // keep the dragged position in the data-x/data-y attributes
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+            //apply transform
+            transform.trans = {
+                x: x,
+                y: y
+            }
+
+            transformIt();
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+
+        },
+        // call this function on every dragend event
+        onend: function (event) {
+
+          var textEl = event.target.querySelector('p');
+
+          textEl && (textEl.textContent =
+            'moved a distance of '
+            + (Math.sqrt(event.dx * event.dx +
+                         event.dy * event.dy)|0) + 'px');
+        }
+      });
+
+
+
+
+
+
+    //use this obj to store transforms values
+    var transform = {
+        rot: { z: 0, y:0, x:0},
+        trans: {x:0, y:0}
+    };
+
+
+
+    //CLICKS AND CHANGES
+
+    $flipV.on('click', function(e){
+        transform.rot.x = ( transform.rot.x==0 ? 180 : 0 );
+        transformIt();
+    });
+
+    $flipH.on('click', function(e){
+        transform.rot.y = ( transform.rot.y==0 ? 180 : 0 );
+        transformIt();
+    });
+
+    $hideFib.on('click', function(e){
+        $fibCont.toggleClass('disable');
+        $(this).html( ( $(this).html()=="Hide" ? "Show" : "Hide"  ) );
+    });
+
+
+    $rotate.find('.inc').on('click',function(){
+        var val = parseFloat($rotate.attr('data-slider')) + .5;
+        $rotate.foundation('slider', 'set_value', val );
+    });
+
+    $rotate.find('.dec').on('click',function(){
+        var val = parseFloat($rotate.attr('data-slider')) - .5;
+        $rotate.foundation('slider', 'set_value', val );
+    });
+
+
+    $rotate.on('change.fndtn.slider', function(e){
+        var deg = parseFloat( $(this).attr('data-slider') );
+            //deg = per * 360 / 100 - 180;
+        transform.rot.z = deg;
+        transformIt();
+    });
+
+
+
+    $sizeFib.find('.inc').on('click',function(e){
+
+        var val = parseFloat($sizeFib.attr('data-slider')) + 0.2;
+        $sizeFib.foundation('slider', 'set_value', val );
+
+    });
+
+
+    $sizeFib.find('.dec').on('click',function(e){
+
+        var val = parseFloat($sizeFib.attr('data-slider')) - 0.2;
+        $sizeFib.foundation('slider', 'set_value', val );
+
+    });
+
+
+    $sizeFib.on('change.fndtn.slider', function(e){
+        var per = parseFloat( $(this).attr('data-slider') ),
+            size = per * ($(window).width() * 1.25) / 100;
+        resizeFib(size);
+    });
+
+
+    //locking stuff
+    var locked = false;
+    $lock.on('click', function(e){
+
+        if(locked === false){
+
+            var proj = map.getProjection();
+            var point = new google.maps.Point($transCont.position().left, $transCont.position().top);
+            var coor = proj.fromPointToLatLng(point);
+
+            var swBound = new google.maps.LatLng(-22.92804166565176, -43.23171615600586);
+            var neBound = new google.maps.LatLng(-22.88503184835787, -43.15824508666992);
+            var bounds = new google.maps.LatLngBounds(swBound, neBound);
+
+
+            overlay = new fibOverlay(bounds, fibDiv, map);
+            $transCont.addClass('hide');
+            locked = true;
+        }
+        else{
+            $transCont.removeClass('hide');
+            overlay.setMap(null);
+            locked = false;
+        }
+
+    });
+
+
+
+
+
+    function resizeFib(val){
+        $fibCont.width(val);
+        $transCont.css({
+            'width': $fibCont.width(),
+            'height': $fibCont.height(),
+        } );
+
+    }
+
+    function tOrigin(){
+      var x,y,
+          l='left',
+          r='right',
+          t='top',
+          b='bottom';
+      if(transform.rot.x == 0 && transform.rot.y == 0){
+        x = l;
+        y = b;
+      }
+      else if(transform.rot.x == 180 && transform.rot.y == 180){
+        x = r;
+        y = t;
+      }
+      else if(transform.rot.x == 180 && transform.rot.y == 0){
+        x = l;
+        y = t;
+      }
+      else if(transform.rot.x == 0 && transform.rot.y ==180 ){
+        x = r;
+        y = b;
+      }
+
+      return {
+        x: x,
+        y: y
+      }
+    }
+
+
+    function transformIt(){
+
+        var origin = tOrigin();
+
+        $fibSvg.css('transform', 'rotateX(' +transform.rot.x+ 'deg) rotateY(' +transform.rot.y+ 'deg)');
+
+        $fibCont.css({
+          'transform': 'rotateZ(' +transform.rot.z+ 'deg)',// translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
+          'transform-origin': tOrigin().x+' '+tOrigin().y
+        });
+
+        $transCont.css({
+            'transform': 'translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
+            'width': $fibCont.width(),
+            'height': $fibCont.height(),
+        } );
+
+    }
 
   //colors
 
--- src/js/overlay.js
+++ src/js/overlay.js
@@ -3,7 +3,8 @@ function fibOverlay(bounds, elem, map) {
 
   // Initialize all properties.
   this.bounds_ = bounds;
-  this.elem_ = elem;
+  this.elem_ = elem.cloneNode(true);
+  this.elem_.id = "cloned-fib";
   this.map_ = map;
 
   // Define a property to hold the image's div. We'll
@@ -47,194 +48,12 @@ fibOverlay.prototype.draw = function() {
 
   // Resize the image's div to fit the indicated dimensions.
   var div = this.div_;
+
+  div.style.transform = 'initial';
   div.style.left = sw.x + 'px';
   div.style.top = ne.y + 'px';
   div.style.width = (ne.x - sw.x) + 'px';
   div.style.height = (sw.y - ne.y) + 'px';
-
-
-    var $fibCont = $('#fib-cont'),
-        $fibSvg = $('#fib-svg'),
-        $fibPath = $('#fib-cont path, #fib-cont use'),
-        $flipV = $('#flipV'),
-        $flipH = $('#flipH'),
-        $rotate = $('#rotate'),
-        $hideFib = $('#hideFib'),
-        $sizeFib = $('#sizeFib'),
-        $transCont = $('#translate-container');
-
-
-
-
-
-    interact('#translate-container')
-      .draggable({
-        onmove: function(event){
-            var target = event.target,
-                // keep the dragged position in the data-x/data-y attributes
-                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-            //apply transform
-            transform.trans = {
-                x: x,
-                y: y
-            }
-
-            transformIt();
-
-            // update the posiion attributes
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
-
-        },
-        // call this function on every dragend event
-        onend: function (event) {
-
-          var textEl = event.target.querySelector('p');
-
-          textEl && (textEl.textContent =
-            'moved a distance of '
-            + (Math.sqrt(event.dx * event.dx +
-                         event.dy * event.dy)|0) + 'px');
-        }
-      });
-
-
-
-
-
-
-    //use this obj to store transforms values
-    var transform = {
-        rot: { z: 0, y:0, x:0},
-        trans: {x:0, y:0}
-    };
-
-
-
-    //CLICKS AND CHANGES
-
-    $flipV.on('click', function(e){
-        transform.rot.x = ( transform.rot.x==0 ? 180 : 0 );
-        transformIt();
-    });
-
-    $flipH.on('click', function(e){
-        transform.rot.y = ( transform.rot.y==0 ? 180 : 0 );
-        transformIt();
-    });
-
-    $hideFib.on('click', function(e){
-        $fibCont.toggleClass('disable');
-        $(this).html( ( $(this).html()=="Hide" ? "Show" : "Hide"  ) );
-    });
-
-
-    $rotate.find('.inc').on('click',function(){
-        var val = parseFloat($rotate.attr('data-slider')) + .5;
-        $rotate.foundation('slider', 'set_value', val );
-    });
-
-    $rotate.find('.dec').on('click',function(){
-        var val = parseFloat($rotate.attr('data-slider')) - .5;
-        $rotate.foundation('slider', 'set_value', val );
-    });
-
-
-    $rotate.on('change.fndtn.slider', function(e){
-        var deg = parseFloat( $(this).attr('data-slider') );
-            //deg = per * 360 / 100 - 180;
-        transform.rot.z = deg;
-        transformIt();
-    });
-
-
-
-    $sizeFib.find('.inc').on('click',function(){
-        var val = parseFloat($sizeFib.attr('data-slider')) + 0.2;
-        $sizeFib.foundation('slider', 'set_value', val );
-    });
-
-    $sizeFib.find('.dec').on('click',function(){
-        var val = parseFloat($sizeFib.attr('data-slider')) - 0.2;
-        $sizeFib.foundation('slider', 'set_value', val );
-    });
-
-    $sizeFib.on('change.fndtn.slider', function(e){
-        var per = parseFloat( $(this).attr('data-slider') ),
-            size = per * ($(window).width() * 1.25) / 100;
-        resizeFib(size);
-    });
-
-
-
-
-
-
-
-
-
-
-    function resizeFib(val){
-        $fibCont.width(val);
-        $transCont.css({
-            'width': $fibCont.width(),
-            'height': $fibCont.height(),
-        } );
-
-    }
-
-    function tOrigin(){
-      var x,y,
-          l='left',
-          r='right',
-          t='top',
-          b='bottom';
-      if(transform.rot.x == 0 && transform.rot.y == 0){
-        x = l;
-        y = b;
-      }
-      else if(transform.rot.x == 180 && transform.rot.y == 180){
-        x = r;
-        y = t;
-      }
-      else if(transform.rot.x == 180 && transform.rot.y == 0){
-        x = l;
-        y = t;
-      }
-      else if(transform.rot.x == 0 && transform.rot.y ==180 ){
-        x = r;
-        y = b;
-      }
-
-      return {
-        x: x,
-        y: y
-      }
-    }
-
-
-    function transformIt(){
-
-        var origin = tOrigin();
-
-        $fibSvg.css('transform', 'rotateX(' +transform.rot.x+ 'deg) rotateY(' +transform.rot.y+ 'deg)');
-
-        $fibCont.css({
-          'transform': 'rotateZ(' +transform.rot.z+ 'deg)',// translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
-          'transform-origin': tOrigin().x+' '+tOrigin().y
-        });
-
-        $transCont.css({
-            'transform': 'translate(' + transform.trans.x + 'px, ' + transform.trans.y + 'px)',
-            'width': $fibCont.width(),
-            'height': $fibCont.height(),
-        } );
-
-    }
-
-
-
 
 
 
