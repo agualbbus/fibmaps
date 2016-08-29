f9da54f64619d675daa51c67db8e3320d97fa5a4
Agustin Albertengo <agustin.albertengo@concrete.com>
2016-08-29 19:47:31 -0300

    switched to hmr 3 beta

--- config/local.webpack.config.babel.js
+++ config/local.webpack.config.babel.js
@@ -16,6 +16,7 @@ export default {
     './main.js',
   ],
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin('[name].[hash].css'),
     new webpack.DefinePlugin({
@@ -48,24 +49,6 @@ export default {
         test: /\.(js|jsx)$/,
         loader: 'babel',
         exclude: /node_modules/,
-        query: {
-          presets: ['react', 'es2015', 'stage-1'],
-          plugins: [['transform-decorators-legacy'], ['transform-object-rest-spread'], ['react-transform', {
-            transforms: [{
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              locals: ['module'],
-            }, {
-              transform: 'react-transform-catch-errors',
-              imports: ['react', 'redbox-react'],
-            }],
-          }]],
-        },
-      },
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel',
-        include: path.resolve(__dirname, '..', 'node_modules', 'react-toaster'),
       },
       {
         test: /\.css$/,
